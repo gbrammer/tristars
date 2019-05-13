6b04727f8ede56ed03cced0fef50dd1b16df36a0
Gabriel Brammer <gbrammer@gmail.com>
2019-05-13 13:32:37 +0200

    Add version, switch to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
+
+version = "0.1" # First tagged version
+
+version_str = """# 
+__version__ = "{0}"\n""".format(version)
+
+fp = open('tristars/version.py','w')
+fp.write(version_str)
+fp.close()
+
 
 setup(name='tristars',
-      version='0.1',
+      version=version,
       description='Match coordinate lists with similar triangles',
       author='Gabriel Brammer',
       author_email='gbrammer@gmail.com',
       url='https://github.com/gbrammer/tristars',
       packages=['tristars'],
       scripts=[],
-      requires=['numpy', 'scipy', 'skimage']
+      install_requires=['numpy', 'scipy', 'scikit-image']
      )
